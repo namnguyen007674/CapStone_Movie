1fbcdb4828257ca515682e03ffbe27159c327836
nam <nvn007674@gmail.com>
2023-10-25 21:36:43 +0700

    backtoTop button

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,16 +1,38 @@
-import React from "react";
-import Header from "../../components/Header";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+// import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import HideAppBar from "../../components/Header/HideNav/HideNav";
-
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import { BackToTop } from "./index";
+import { Outlet } from "react-router-dom";
 export default function MainLayout() {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const handleBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <>
-      <HideAppBar/>
+      <HideAppBar />
       <Outlet />
-      <Footer/>
+      <Footer />
+      {showBackToTop && (
+        <BackToTop onClick={handleBackToTop} className="back-to-top-button">
+          <ExpandLessIcon sx={{ fontSize: "80px" }} />
+        </BackToTop>
+      )}
     </>
-    
   );
 }
