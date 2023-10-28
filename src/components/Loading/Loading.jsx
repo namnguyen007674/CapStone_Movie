40a0fbe333a68724bf974597c45e9851e2f7093d
nam <nvn007674@gmail.com>
2023-10-28 19:01:02 +0700

    responsive loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,14 +4,14 @@ import { DivCustom } from "./index";
 
 export default function Loading(){
   return (
-    <Container sx={{ height: "70vh" }}>
+    <Container >
       <DivCustom>
         <img
           src="/img/animation_lmnjb4q6_small.gif"
           alt="Loading-animation"
-          width={300}
+          width={200}
         />
-        <Typography variant="h4">Vui Lòng Đợi...</Typography>
+        <Typography sx={{fontSize:'16px'}} variant="h4">Vui Lòng Đợi...</Typography>
       </DivCustom>
     </Container>
   );
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -3,6 +3,9 @@ import styled from "@emotion/styled";
 export { default } from "./Loading";
 
 export const DivCustom = styled.div`
-  margin-top: 15%;
-  margin-left: 35%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  flex-direction: column;
 `;
